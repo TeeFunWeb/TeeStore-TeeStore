7cc8b26917435a2d76852b9f15b2fb6897567f58
Ninecloud2077 <ncjack2077@163.com>
2022-07-24 17:07:25 +0800

    初始化

---
+++ TeeStore.py
@@ -0,0 +1,33 @@
+import pgzrun
+import requests
+
+WIDTH=950
+HEIGHT=550
+
+class Button:
+    FontSize=45
+    FontName=None
+    def __init__(self,Pos,Text,Callback=None,Img='button'):
+        self.Actor=Actor(Img,Pos)
+        self.Pos=Pos
+        self.Text=Text
+        self.Callback=Callback
+
+        self.Rect=Rect(self.Actor.topleft,self.Actor.bottomright)
+    
+    def draw(self):
+        self.Actor.draw()
+        
+        screen.draw.text(self.Text,self.Pos,color='black',fontsize=Button.FontSize)
+    
+    def mousedown(self):
+        if self.Callback:
+            self.Callback()
+
+Test=Button((0,0),'HW')
+
+def draw():
+    screen.fill((255,255,255))
+    Test.draw()
+
+pgzrun.go(Title='TeeUniverse:Teestore')
