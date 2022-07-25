8c9794c201720954feaa63788ee500c2b5a4a7d7
Ninecloud2077 <ncjack2077@163.com>
2022-07-25 16:45:17 +0800

    面板

--- TeeStore.py
+++ TeeStore.py
@@ -30,6 +30,38 @@ class Button:
             State=self.Callback
 
 
+class LifeText: #文本的类
+    def __init__(self,Msg,Pos,Size=20,LifeTime=-1): #文本参数
+        self.Msg=Msg #文本
+        self.Pos=Pos #位置
+        self.LifeTime=LifeTime #持续时间，-1表示永久存在
+        self.Size=Size #大小
+        self.Death=0 #时间是否结束
+
+    def draw(self): #绘制文本部分
+            screen.draw.text(self.Msg,self.Pos,color='black',fontsize=self.Size) #依据文本、位置、大小绘制黑色文本
+            
+    def up(self): #文本更新部分
+        self.LifeTime-=1 #持续时间-1
+        if not self.LifeTime: #如果时间到了
+            self.Death=1 #表示时间结束
+
+
+class Panel:
+    def __init__(self,No,Pos=(WIDTH//3+50,50),End=(WIDTH//3*2-100,HEIGHT-100),Color=(255,255,255)):
+        self.No=No
+        self.Pos=Pos
+        self.Rect=Rect(Pos,End)
+        self.Color=Color
+        self.Base=[]
+    
+    def draw(self):
+        global State
+        if self.No!=State:
+            return
+        screen.draw.filled_rect(self.Rect,self.Color)
+    
+
 
 Buttons=[]
 Labels=['下载启动器','选择模式','开始游戏']
@@ -40,23 +72,21 @@ for i in range(1,4):
     i)
     Buttons.append(b)
 
+Panels=[]
+for i in range(1,4):
+    Panels.append(Panel(i))
+
 
 State=0 #0:Nope 1:download 2:choice 3:start
-'''
-def chstate(v):
-    global State
-    State=v
-    if v==3:
-'''
 
 
 def draw():
     screen.fill((220,220,220))
     screen.draw.line((WIDTH//3,0),(WIDTH//3,HEIGHT),(155,155,155))
 
-    for i in Buttons:
+    for i in Buttons+Panels:
         i.draw()
-
+    
 
 def on_mouse_down(pos):
     for i in Buttons:
